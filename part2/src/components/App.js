512a1b110ab1f071ddfb3f5f54a137ad822e2a8f
Saibot101 <tobiasmaas98@web.de>
2020-04-17 14:28:56 +0200

    Part_2c Part2 finished

--- part2/src/components/App.js
+++ part2/src/components/App.js
@@ -1,93 +1,98 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Note from './Note'
+
 
 const App = () => {
-    const [ persons, setPersons] = useState([
-    ]);
-    const [ newName, setNewName ] = useState('');
-    const [newNumber, setNewNumber] = useState('');
-    const [showPersons, setPersonsShow] = useState(true);
+    const [ countries, setCountries] = useState([]);
+
+    const [showCountries, setShowCountries] = useState(true);
     const [search, setSearch] = useState('');
+    const [searchedCountries, setSearchedCountries] = useState([{name: "search for countries"}])
 
     useEffect(() => {
-        axios.get('http://localhost:3001/persons')
+        axios.get('https://restcountries.eu/rest/v2/all')
             .then(response => {
-                setPersons(response.data)
+                setCountries(response.data)
+                console.log(response.data)
             })
     }, [])
 
-    const addName = (event) => {
-        event.preventDefault();
-
-
-        for( let i = 0; i<persons.length;i++){
-            if (persons[i].name === newName || persons[i].number === newNumber){
-                window.alert(`${newName} or ${newNumber} is already in the phonebook`);
-            }
-            else{
-                const nameObject = {
-                    name: newName,
-                    number: newNumber,
-                    id: persons.length+1
-                };
-
-                setPersons(persons.concat(nameObject));
-                setNewName('')
-                setNewNumber('')
-            }
-        }
-
-
-    };
-
-    const handleNameChange = (event) => {
-        setNewName(event.target.value)
-    };
-
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value)
-    };
 
     const changeHandler = (event) => {
         setSearch(event.target.value)
 
         if(event.target.value !== ""){
-            setPersonsShow(false);
-            console.log(false)
+            setShowCountries(false);
+            if(countries.filter(countrie => countrie.name.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1).length > 10){
+                setSearchedCountries([{name: "more then 10 countries"}])
+                console.log('to many')
+            } else {
+                setSearchedCountries(countries.filter(countrie => countrie.name.toLowerCase().indexOf(event.target.value.toLowerCase())> -1))
+                console.log('under 10')
+            }
         } else {
-            setPersonsShow(true)
+            setShowCountries(true)
+            setSearchedCountries([{name: "search for countries"}])
             console.log(true)
+        }
+    };
+
+    const Countries = () => {
+        if(searchedCountries.length === 1){
+
+            if(searchedCountries[0].name === "search for countries" || searchedCountries[0].name === "more then 10 countries"){
+                return (
+                    <div>
+                        <p>{searchedCountries[0].name}</p>
+                    </div>
+
+                )
+            }else {
+                console.log(searchedCountries[0])
+                return (
+                    <div>
+                        <CountrieView/>
+                    </div>
+                )
+            }
+        } else {
+            return (
+                <div>
+                    <ul>
+                        {searchedCountries.map( countrie => [
+                            <p key={countrie.id}> {countrie.name}</p>,
+                            <button key={countrie.name} type={"submit"} onClick={() => {setSearch(countrie.name); setSearchedCountries([countrie])}}>{countrie.name} show</button> ]
+                        )
+                        }
+
+                    </ul>
+                </div>
+            )
         }
     }
 
-    const personsToShow = showPersons ? persons : persons.filter( person => person.name.toLowerCase().indexOf(search.toLowerCase()) > -1 );
+    const CountrieView = () => {
+        return(
+            <div>
+                <h1>{searchedCountries[0].name}</h1>
+                <p>Capital: {searchedCountries[0].capital}</p>
+                <p>Population: {searchedCountries[0].population}</p>
+                <h3>Languages</h3>
+                <ul>
+                    {searchedCountries[0].languages.map(language => <p key={language.name}>{language.name}</p>)}
+                </ul>
+                <img src={searchedCountries[0].flag}  alt={"empty"} width={150} height={150}/>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>Phonebook</h2>
             <p>filter shown with </p>
             <input value={search} onChange={changeHandler}/>
-            <form onSubmit={addName}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange} />
-                    number: <input value={newNumber} onChange={handleNumberChange}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <div>
-                <ul>
-                    {personsToShow.map( person =>
-                        <p key={person.name}>{person.name}  {person.number}</p>,
 
-                    )
-                    }
-
-                </ul>
-            </div>
+            <h2>Countries</h2>
+            <Countries />
 
         </div>
     )
